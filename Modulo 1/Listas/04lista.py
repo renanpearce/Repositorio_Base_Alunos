6bf186f8e0518ef5b24b7ad656f3011d0d115548
renanpearce <pearce2renan@gmail.com>
2025-09-10 16:16:41 -0300

    Add files via upload

---
+++ Modulo 1/Listas/04lista.py
@@ -0,0 +1,13 @@
+# 4. remove()
+#Dada a lista cores = ["vermelho", "azul", "verde", "azul"], use
+#.remove() para retirar apenas a primeira ocorrência de "azul". Mostre a lista
+#antes e depois.
+
+cores = ["vermelho","azul","verde","azul"]
+print(cores)
+
+cores.remove("azul")
+print(cores)
+
+
+
